d960e8f5661be3550e9582b90fb9c80b6680871c
Priptide <priptidehelp@gmail.com>
2022-08-22 18:16:32 +0100

    setup for two planets

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import "./App.css";
+import "tailwindcss/tailwind.css";
+
+import * as THREE from "three";
 
 import React, { useEffect } from "react";
 
@@ -9,10 +11,27 @@ function App() {
     let test = new SceneInit();
     test.initScene();
     test.animate();
+
+    const sunGeometry = new THREE.SphereGeometry(8, 32, 32);
+    const sunMaterial = new THREE.MeshNormalMaterial();
+    const sunMesh = new THREE.Mesh(sunGeometry, sunMaterial);
+    sunMesh.position.set(10, 0, 0);
+    sunMesh.castShadow = true;
+    const solarSystem = new THREE.Group();
+    solarSystem.add(sunMesh);
+
+    const moonGeometry = new THREE.SphereGeometry(8, 32, 32);
+    const moonMaterial = new THREE.MeshNormalMaterial();
+    const moonMesh = new THREE.Mesh(moonGeometry, moonMaterial);
+    moonMesh.position.set(-10, 0, 0);
+    moonMesh.castShadow = true;
+    solarSystem.add(moonMesh);
+    test.scene?.add(solarSystem);
   });
+
   return (
-    <div className="App">
-      <canvas id="threeJsCanvas" />
+    <div className="flex flex-col items-center justify-center">
+      <canvas id="myThreeJsCanvas" />
     </div>
   );
 }
